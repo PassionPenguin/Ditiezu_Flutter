b21d60a4750aeb70f958c8c8f74544a9440ae8db
霜羽 HoarfrostFeather <penguin.zhang@qq.com>
2020-10-25 16:20:13 +0800

    Extract Interactive Page Actions -> InteractivePage.dart

---
+++ lib/Widgets/InteractivePage.dart
@@ -0,0 +1,121 @@
+import 'package:Ditiezu/Widgets/w_iconMessage.dart';
+import 'package:flutter/material.dart';
+
+abstract class InteractivePage {
+  // Interactive Indicators
+  bool _isLoading = false;
+  bool _isMessageShowing = false;
+  String _message = "";
+  IconData _icon = Icons.check;
+  Color _color = Colors.green;
+  Map<String, Animation<double>> __fadeAnimation = {};
+  Map<String, AnimationController> __fadeController = {};
+
+  bool get isLoading {
+    return _isLoading;
+  }
+
+  set isLoading(loadingState) {
+    _isLoading = loadingState;
+  }
+
+  bool get isMessageShowing {
+    return _isMessageShowing;
+  }
+
+  set isMessageShowing(messageState) {
+    _isMessageShowing = messageState;
+  }
+
+  get fadeController {
+    return __fadeController;
+  }
+
+  set fadeController(ctrl) {
+    __fadeController = ctrl;
+  }
+
+  get fadeAnimation {
+    return __fadeAnimation;
+  }
+
+  set fadeAnimation(anim) {
+    __fadeAnimation = anim;
+  }
+
+  IconMessage icnMessage() {
+    return IconMessage(icon: _icon, color: _color, message: _message);
+  }
+
+  void setAnim(bool isLoading, bool isMessageShowing, String message, Color color, IconData icon) {
+    if (isMessageShowing) {
+      fadeController["main"].reverse();
+      fadeController["messaging"].forward();
+      if (_loadingEnabled) fadeController["loading"].reverse();
+      print(fadeAnimation["messaging"]);
+      print(fadeAnimation["loading"]);
+      print(fadeAnimation["main"]);
+    } else if (isLoading && _loadingEnabled) {
+      fadeController["loading"].forward();
+      fadeController["main"].reverse();
+      fadeController["messaging"].reverse();
+    } else {
+      fadeController["main"].forward();
+      fadeController["messaging"].reverse();
+      if (_loadingEnabled) fadeController["loading"].reverse();
+    }
+    if (_parentState.mounted) {
+      // ignore: invalid_use_of_protected_member
+      _parentState.setState(() {
+        _isLoading = isLoading;
+        _isMessageShowing = isMessageShowing;
+        _message = message;
+        _color = color;
+        _icon = _icon;
+      });
+      if (isMessageShowing)
+        Future.delayed(Duration(seconds: 2), () {
+          // ignore: invalid_use_of_protected_member
+          _parentState.setState(() {
+            _isMessageShowing = false;
+            fadeController["main"].forward();
+            fadeController["messaging"].reverse();
+            if (_loadingEnabled) fadeController["loading"].reverse();
+          });
+        });
+      // ignore: invalid_use_of_protected_member
+    }
+  }
+
+  // Preferences
+  bool _loadingEnabled;
+
+  // State
+  State _parentState;
+  TickerProvider _tickerProvider;
+
+  @mustCallSuper
+  @protected
+  void bindIntractableWidgets(bool loadingEnabled, parentState) {
+    _loadingEnabled = loadingEnabled;
+    _parentState = parentState;
+    _tickerProvider = parentState;
+
+    fadeController["main"] = new AnimationController(vsync: _tickerProvider, duration: Duration(seconds: 1));
+    fadeController["messaging"] = new AnimationController(vsync: _tickerProvider, duration: Duration(seconds: 1));
+    fadeController["loading"] = new AnimationController(vsync: _tickerProvider, duration: Duration(seconds: 1));
+    fadeAnimation["main"] = new Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(fadeController["main"]);
+    fadeAnimation["messaging"] = new Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(fadeController["messaging"]);
+    fadeAnimation["loading"] = new Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(fadeController["loading"]);
+    fadeController["main"].forward();
+  }
+}
